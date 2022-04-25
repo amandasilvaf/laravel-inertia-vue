c575a7e8843d19617b25249b524a683bdfd9715e
Amanda Ferreira <amandasilvaf1995@gmail.com>
2022-04-25 17:26:37 -0300

    First commit

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+      <div>Página Sobre</div>
+  </Layout>
+
+</template>
+
+<script>
+import Layout from "../Layout"; 
+export default{
+  components: { Layout }
+
+};
+</script>
+
+<style>
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+      <div>Página Home</div>
+  </Layout>
+
+</template>
+
+<script>
+import Layout from "../Layout"; 
+
+export default{
+  components: { Layout }
+
+};
+</script>
+
+<style>
+</style>
